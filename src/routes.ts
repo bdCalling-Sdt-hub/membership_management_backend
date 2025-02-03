acc3991d51556dc50885c6cd0e5260bb09c114bb
safin-calling <safin248@outlook.com>
2025-02-03 18:06:33 +0600

    planning

--- src/routes.ts
+++ src/routes.ts
@@ -16,12 +16,61 @@ export default function (app: Express) {
   app.post("/forgot-password", forgot_password);
   app.post("/update-password", update_password);
   app.post("/sign-in", signin);
-  //   app.post("/forgot-password");
-  //   app.post("/reset-password");
-  //   app.get("/profile");
-  //   app.put("/update-profile");
+
+  // HOME
+  // app.get("/overview")
+  // - user info
+  // - earnings
+  // - traffic data based on dates
+  // - referral history
 
   // TOOLS
-  //   app.get("/tools");
-  //   app.post("/add-tools");
+  // app.get("/tools");
+  // app.get("/tools/:id");
+
+  // NOTIFICATIONS
+  // app.get("/notifications")
+
+  // WALLET
+  // app.get("/withdraw-history")
+
+  // PROFILE
+  // app.get("/profile");
+  // app.put("/update-profile");
+  // app.delete("/delete-account")
+
+  // ------------------------------
+
+  // DASHBOARD
+  // app.get("/dashboard")
+  // app.get("/referral-overview")
+  // app.get("/admin-notifications")
+
+  // USER MANAGEMENT
+  // app.get("/users")
+  // app.post("/ban-user")
+
+  // TOOLS MANAGEMENT
+  // app.get("/admin-tools")
+  // app.put("/admin-tools")
+  // app.delete("/admin-tools")
+
+  // EARNINGS
+  // app.get("/earnings")
+  
+  // WITHDRAW REQUEST
+  // app.get("/withdraw-requests")
+
+  // REFERRAL COMMISSION
+  // app.get("/referral-commissions")
+  // app.put("/referral-commissions")
+  // app.delete("/referral-commissions")
+
+  // TOOLS CATEGORY
+  // app.get("/tools-category")
+  // app.put("/tools-category")
+  // app.delete("/tools-category")
+
+  // PROFILE
+  // app.post("/change-password")
 }
