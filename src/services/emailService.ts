1c3ba7e406ebe457ed69bbc7b20aa2becba03ebe
safin-calling <safin248@outlook.com>
2025-02-04 10:24:02 +0600

    nodemailer issue fixed

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -8,9 +8,7 @@ interface EmailOptions {
 }
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.office365.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
